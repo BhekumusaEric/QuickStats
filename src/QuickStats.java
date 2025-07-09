80743ece50782a5cdaa6b185c59b6679022cbe91
BhekumusaEric <bhekumusaeric@gmail.com>
2025-07-09 12:26:02 +0200

    my first Java project

---
+++ src/QuickStats.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import  java.util.Arrays;
+
+public class QuickStats {
+    public static boolean isNumeric(String val){
+        if (val == null){
+            return  false;
+        }
+        try {
+            Integer.parseInt(val);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public  static void main(String[] args){
+        System.out.println(args.length);
+        String[] myArgs = args;
+        System.out.println(Arrays.toString(myArgs));
+        ArrayList<String> dynamic_lst = new ArrayList<>();
+        int intTotal = 0;
+        for (int counter = 0; counter < args.length; counter ++){
+            System.out.println(myArgs[counter]);
+            if (isNumeric((String)myArgs[counter])){
+                dynamic_lst.add(myArgs[counter]);
+                intTotal += Integer.parseInt(myArgs[counter]);
+                continue;
+            }
+            dynamic_lst.add(myArgs[counter] + " (ignored)");
+        }
+        System.out.println(dynamic_lst);
+        System.out.println("Count :" + dynamic_lst.toArray().length);
+        System.out.println("Total :" + intTotal);
+        System.out.println("Average :" + (float)(intTotal / dynamic_lst.toArray().length));
+    }
+}
